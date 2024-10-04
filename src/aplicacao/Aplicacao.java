2aaf401001555ca4a8ca4f0ab769338a8ec43ae8
Danielelucena <165947071+Danielelucena@users.noreply.github.com>
2024-10-04 14:48:05 -0300

    Update Aplicacao.java

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -15,14 +15,18 @@ public class Aplicacao extends Aluno {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Deseja se cadastrar: ");
-
+        System.out.println("Deseja se cadastrar ");
         System.out.println("Digite seu nome:");
         String nome = sc.nextLine();
         System.out.println("Digite sua idade:");
-        int idade = sc.nextInt();
-        Pessoa pessoa = new Pessoa(nome, idade);
-        pessoa.mostrarDados(nome,idade);
+        String idade = sc.nextLine();
+        System.out.println("Digite seu numero de Matricula:");
+        String numeroMatricula = sc.nextLine();
+        System.out.println("Digite seu curso desejado");
+        String nomeCurso = sc.nextLine();
+
+        Pessoa pessoa = new Pessoa(nome, idade, numeroMatricula, nomeCurso);
+        pessoa.mostrarDados(nome,idade,numeroMatricula,nomeCurso);
 
     }
 }
